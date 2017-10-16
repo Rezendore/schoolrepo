caf9816da9fcbd9638524a474295c86cc66bb0c7
Rezendore <mailhotsj@gmail.com>
2017-10-16 13:58:27 -0400

    b
    b

--- appFrameGame/app.js
+++ appFrameGame/app.js
@@ -5,23 +5,21 @@ function MyApp()
 {
 	var guess = 0;
 	var answer = Math.floor((Math.random() * 100) + 0);
-	function test()
-	{
-		guess = document.getElementById("guess").value;
 	
-		if(guess == answer)
-		{
-			document.getElementById("result").innerHTML="Correct!";
-		}else if(guess > answer)
-		{
-			document.getElementById("result").innerHTML="Too high!";
-		}
-		else
-		{
-			document.getElementById("result").innerHTML="Too low!";
-		}
+	guess = document.getElementById("guess").value;
+	
+	if(guess == answer)
+	{
+		document.getElementById("result").innerHTML="Correct!";
+	}else if(guess > answer)
+	{
+		document.getElementById("result").innerHTML="Too high!";
 	}
-
+	else
+	{
+		document.getElementById("result").innerHTML="Too low!";
+	}
+	
 	var version = "v1.0";
 
 	// creating a private function
