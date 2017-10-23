8046747bc9f41804fb9de1adf22785d93a781e5a
Rezendore <mailhotsj@gmail.com>
2017-10-22 22:32:49 -0400

    c

--- taskAtHand1a/taskAtHand.js
+++ taskAtHand1a/taskAtHand.js
@@ -27,15 +27,20 @@ function TaskAtHandApp()
 	
 	function addTaskElement(taskName)
 	{
-		var $task = $("<li></li>");
-		var $delete = $("<button class='delete'>X</button>");
-		var $moveUp = $("<button class='move-up'>^</button>");
-		var $moveDown = $("button class'move-down'>v</button>");
-		$task.append($delete).append($moveUp).append($moveDown).append("<span class='task-name'>" + taskName " "</span>");
-		$("#task-list").append($task);
-		$delete.click(function() { $task.remove(); });
-		$moveUp.click(function() { $task.insertBefore($task.prev()); });
-		$moveDown.click(function() { $task.insertAfter($task.next()); });
+		 var $task = $("<li></li>");
+		 var $delete = $("<button class='delete'>X</button>");
+		 var $moveUp = $("<button class='move-up'>^</button>");
+		 var $moveDown = $("<button class='move-down'>v</button>");
+		 $task.append($delete)
+		 .append($moveUp)
+		 .append($moveDown)
+		 .append("<span class='task-name'>" + taskName +
+		 "</span>");
+		 $("#task-list").append($task);
+
+		 $delete.click(function() { $task.remove(); });
+		 $moveUp.click(function() { $task.insertBefore($task.prev()); });
+		 $moveDown.click(function() { $$task.insertAfter($task.next()); });
 	}
 }
 $(function() {
